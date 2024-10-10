83d7776afd27ace1d2d010d78b51de1269c4c98f
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-10-09 20:28:09 -0500

    ValidacionTipoCuenta
    ValidacionTipoCuenta

--- ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -19,13 +19,20 @@ namespace ManejoPresupuesto.Controllers
         }
 
         [HttpPost]
-        public IActionResult Crear(TipoCuenta tipoCuenta)
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
         {
             if (!ModelState.IsValid)
             {
                 return View(tipoCuenta);
             }
-            repositorioTiposCuentas1.crear(tipoCuenta);
+            var yaExisteTipoCuenta = await repositorioTiposCuentas1.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+
+            if (yaExisteTipoCuenta)
+            {
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre), $"El nombre {tipoCuenta.Nombre} ya existe.");
+                return View(tipoCuenta);
+            }
+             await repositorioTiposCuentas1.crear(tipoCuenta);
 
             return View(tipoCuenta);
 
