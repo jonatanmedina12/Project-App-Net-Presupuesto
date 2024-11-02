86537a059e14650487ec54a32aad54b6b9fb7c3f
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-11-01 22:10:43 -0500

    Cuenta
    Cuenta

---
+++ ManejoPresupuesto/ManejoPresupuesto/Models/Cuenta.cs
@@ -0,0 +1,23 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Cuenta
+    { 
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength:50)]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+
+        [Display(Name ="Tipo Cuenta")]
+        public string TipoCUentaId { get; set; }
+        
+        public decimal Balance { get; set; }
+
+        [StringLength(maximumLength:1500)]
+        public string Descripcion { get; set; }
+    }
+}
