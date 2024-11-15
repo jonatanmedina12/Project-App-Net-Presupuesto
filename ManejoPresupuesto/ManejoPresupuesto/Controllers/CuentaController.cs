0f9bb5a2904332643f4be7ae8341ebba48bf46c6
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-11-14 22:16:38 -0500

    1
    1

--- ManejoPresupuesto/ManejoPresupuesto/Controllers/CuentaController.cs
+++ ManejoPresupuesto/ManejoPresupuesto/Controllers/CuentaController.cs
@@ -2,6 +2,7 @@
 using ManejoPresupuesto.Servicios;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ManejoPresupuesto.Controllers
 {
@@ -10,20 +11,45 @@ namespace ManejoPresupuesto.Controllers
     {
         private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
         private readonly IservicioUsuarios iservicioUsuarios;
+        private readonly IrepositorioCuentas irepositorioCuentas;
 
-        public CuentaController(IRepositorioTiposCuentas repositorioTiposCuentas,IservicioUsuarios iservicioUsuarios) {
+        public CuentaController(IRepositorioTiposCuentas repositorioTiposCuentas, IservicioUsuarios iservicioUsuarios, IrepositorioCuentas irepositorioCuentas) {
             this.repositorioTiposCuentas = repositorioTiposCuentas;
             this.iservicioUsuarios = iservicioUsuarios;
+            this.irepositorioCuentas = irepositorioCuentas;
         }
         [HttpGet]
-        public async Task< IActionResult> Crear()
+        public async Task<IActionResult> Crear()
         {
             var usuarioId = iservicioUsuarios.ObtenerUsuariosId();
-            var tiposCuentas = await repositorioTiposCuentas.Obtener("",usuarioId);
+            var tiposCuentas = await repositorioTiposCuentas.Obtener("", usuarioId);
             var modelo = new CuentaCreacionViewModel();
             modelo.TiposCuentas = tiposCuentas.Select(x => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem(x.Nombre, x.Id.ToString()));
             return View(modelo);
 
+        }
+        [HttpPost]
+        public async Task<IActionResult> Crear(CuentaCreacionViewModel cuenta)
+        {
+            var usuarioId =  iservicioUsuarios.ObtenerUsuariosId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener("", usuarioId);
+            if(tiposCuentas is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            if (!ModelState.IsValid)
+            {
+                cuenta.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
+                return View(cuenta);
+            }
+            await irepositorioCuentas.crear(cuenta);
+            return View(cuenta);
+        }
+        private async Task<IEnumerable<SelectListItem>>ObtenerTiposCuentas(int usuarioId)
+        {
+            var tiposCuentas = await repositorioTiposCuentas.Obtener("", usuarioId);
+
+            return tiposCuentas.Select(x => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem(x.Nombre, x.Id.ToString())); ;
         }
     }
 }
---
+++ ManejoPresupuesto/ManejoPresupuesto/Servicios/repositorioCuentas.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{   
+    public interface IrepositorioCuentas
+    {
+        Task crear(Cuenta cuenta);
+    }
+    public class repositorioCuentas : IrepositorioCuentas
+    {
+        private readonly string connectionString;
+
+        public repositorioCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        }
+        public async Task crear(Cuenta cuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync(@"INSERT INTO cuentas (Nombre,TipoCuentaId,Descripcion,Balance)Values (@Nombre,@TipoCuentaId,@Description,@Balance);
+                Selectop SCOPE_IDENTITY();",cuenta);
+
+        }
+    }
+}
